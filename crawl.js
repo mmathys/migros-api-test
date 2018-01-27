2bcc7cfafdfa09f49a142866bccca4a768c649d6
Max Mathys <threadox@gmail.com>
2018-01-27 14:36:43 +0100

    optimizations, should actually study diskmath

--- crawl.js
+++ crawl.js
@@ -7,18 +7,23 @@ var limit = 400;
 var done = false;
 var count = 0;
 var ddosSafe = 1000;
+var last = "";
 
 function next(error, response, body) {
-    console.log('error:', error); // Print the error if one occurred
+    if (error) console.log('error:', error); // Print the error if one occurred
     if (error) done = true;
 
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     if (body) {
         body = JSON.parse(body);
         //console.log('body:', body); // Print the HTML for the Google homepage.
 
+
         for (var i = 0; i < body.length; i++) {
-            console.log(body[i].name);
+            if (body[i].name !== last) {
+                console.log(body[i].name);
+                last = body[i].name;
+            }
         }
     }
 
